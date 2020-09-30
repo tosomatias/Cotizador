ec4d14a202969d7aac71ee8a1a05684f5a7b3670
matute <tosomatias@gmail.com>
2020-09-29 21:00:01 -0300

    Updates

---
+++ precache-manifest.2def8f95f442a45b6a6024b2080fa6a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad477389e7ddeb7c2b06a8248de8224b",
+    "url": "/Cotizador-Bitcoins/index.html"
+  },
+  {
+    "revision": "11dcd65de9af1ce4070a",
+    "url": "/Cotizador-Bitcoins/static/css/main.7b2a76e1.chunk.css"
+  },
+  {
+    "revision": "d40dd198fc7a573faad4",
+    "url": "/Cotizador-Bitcoins/static/js/2.608add4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cotizador-Bitcoins/static/js/2.608add4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11dcd65de9af1ce4070a",
+    "url": "/Cotizador-Bitcoins/static/js/main.c8df49aa.chunk.js"
+  },
+  {
+    "revision": "c7f671e8f5215cc28937",
+    "url": "/Cotizador-Bitcoins/static/js/runtime-main.cf175401.js"
+  }
+]);
